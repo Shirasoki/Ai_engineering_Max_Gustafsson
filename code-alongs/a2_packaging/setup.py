45cc95e036bff182971989e52fe90c3c6fd54954
Shirasoki <maxleon_lappalainen@hotmail.com>
2025-09-17 11:17:36 +0200

    working on setup tools

---
+++ code-alongs/a2_packaging/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+print(find_packages())
+
+setup(
+    name="cool_package",
+    version="0.0.1",
+    description="this package is a template for fullstack app ",
+    author="Max Gustafsson",
+    author_email="author@mail.se",
+    packages=find_packages(),
+)
